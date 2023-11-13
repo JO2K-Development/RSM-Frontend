6d5be49d2eec282aafbb213393eb13f22908eb76
Karol Zając <karolzajac.0407@gmail.com>
2023-11-13 17:27:20 +0100

    router and  store init

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { store } from './redux/store';
+import { Provider } from 'react-redux'; 
+import {RouterProvider,   createBrowserRouter} from 'react-router-dom';
 
-const root = ReactDOM.createRoot(
+
+const router=createBrowserRouter([{path:'/', element: <div>Home</div>}])
+
+
+ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
-);
-root.render(
+).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <RouterProvider router={router}/>
+    </Provider>
   </React.StrictMode>
 );
+
 
